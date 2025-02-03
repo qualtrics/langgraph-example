1d56ce033cba3231af08b4b16917682dd1649a58
Qualtrics Developer Advocacy <61558050+dev-advocacy-qualtrics@users.noreply.github.com>
2025-02-03 15:59:07 -0800

    Update tools.py

--- my_agent/utils/tools.py
+++ my_agent/utils/tools.py
@@ -1,3 +1,12 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
+import requests
 
-tools = [TavilySearchResults(max_results=1)]
+url = 'https://pdx1.qualtrics.com/inbound-event/v1/events/json/triggers?urlTokenId=603bfb63-acb9-4cd4-9448-8b93a4475c62'
+
+@tool
+def postSlackMessage(query: str):
+  response = requests.post(url, data=query)
+  return "success"
+
+tools = [TavilySearchResults(max_results=1), postSlackMessage]
