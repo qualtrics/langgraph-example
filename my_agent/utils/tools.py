4ee1dbb82e6ce2c652b11476607615c8764e73ae
Qualtrics Developer Advocacy <61558050+dev-advocacy-qualtrics@users.noreply.github.com>
2025-02-03 15:54:18 -0800

    add slack posting tool

--- my_agent/utils/tools.py
+++ my_agent/utils/tools.py
@@ -1,3 +1,11 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
+import requests
 
-tools = [TavilySearchResults(max_results=1)]
+url = 'https://pdx1.qualtrics.com/inbound-event/v1/events/json/triggers?urlTokenId=603bfb63-acb9-4cd4-9448-8b93a4475c62'
+
+@tool
+def postSlackMessage(query: str):
+  response = requests.post(url, data=query)
+  return "success"
+
+tools = [TavilySearchResults(max_results=1), postSlackMessage]
